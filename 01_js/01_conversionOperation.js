64d407abebb54f3c93039d78d3853207e2fade96
Lepharam Ramchiary <89240335+LepharamRamchiary@users.noreply.github.com>
2024-08-12 06:41:23 +0000

    conversion of data type

---
+++ 01_js/01_conversionOperation.js
@@ -0,0 +1,42 @@
+// Number
+
+// let age = null
+// let age = undefined
+// let age = true => 1; false => 0
+// let age = "Lepharam"
+// let age = "23av" => NaN
+// let age = "23" => 23
+
+// console.log(typeof (age))
+
+// let valueInNumber = Number(age)
+
+// console.log( typeof valueInNumber);
+// console.log( valueInNumber);
+
+
+// Boolean
+// let isLoggedIn = 1, => true
+// let isLoggedIn = "", => false
+// let isLoggedIn = 8, => true
+// let isLoggedIn = "lepharam", => true
+// let isLoggedIn = null, => false
+// let isLoggedIn = undefined, => false
+// let isLoggedIn = 0, => false
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+
+// Srting 
+// let age = 33, => 33
+// let age = "33", => 33
+// let age = null, => null
+// let age = undefined, => undefined
+
+
+let numberToString = String(age)
+console.log(typeof numberToString);
+console.log(numberToString);
+
+
