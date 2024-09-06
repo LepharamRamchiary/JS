eb8b738eb196078d0e82e877d4b80bafc70c49e0
Lepharam Ramchiary <89240335+LepharamRamchiary@users.noreply.github.com>
2024-09-06 07:26:31 +0000

    number methods

---
+++ 01_js/number.js
@@ -0,0 +1,25 @@
+const num1 = 132452
+
+const num2 = new Number(200)
+
+console.log(num1);
+console.log(num2);
+
+
+const convertStr = num2.toString()
+// console.log(convertStr);
+// console.log(convertStr.length);
+
+const decimal = 1000
+// console.log(decimal.toFixed(2));
+
+const amount = 10000000000000
+// console.log(amount.toLocaleString("en-IN"));
+
+const decimalNumber = 1000.888999
+// console.log(decimalNumber.toPrecision(5));
+
+
+const type = 100
+// console.log(type.valueOf());
+
