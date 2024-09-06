67e5b7be211ae45597365edfd01a6d4268909640
Lepharam Ramchiary <89240335+LepharamRamchiary@users.noreply.github.com>
2024-09-06 07:00:37 +0000

    practiching some string methods

---
+++ 01_js/string.js
@@ -0,0 +1,47 @@
+const name = "Lepharam"
+const countRepo = 40
+console.log(`Hi my name is ${name} and i heve ${countRepo} repo in my github`);
+
+const url = new String("https//lepharamramchiary%20lepha-20.com")
+const url2 = new String("https//lepharamramchiary%20lepha%20.com")
+
+// console.log(url.charAt(7));
+// console.log(url.indexOf("l"));
+// console.log(url.replace("%20", ""));
+// console.log(url.replace("-20", ""));
+// console.log(url.replace(/%20|-20/g, ""));
+
+// console.log(url.repeat(3));
+// console.log(url2.replaceAll("%20", "-dog-"));
+
+// console.log(url.slice(0, 4));
+// console.log(url.slice(1,-1));
+
+
+const city = "I am from gogamukh, and you are"
+
+// const words = city.split(" ")
+// console.log(words[3]);
+
+// console.log(city.split(" ", 2));
+
+// console.log(city.substring(0, 10));
+
+const age = 23
+// console.log(age.toString());
+
+const schoolName = "    Gogamukh HS School   " 
+
+console.log(schoolName);
+console.log(schoolName.trim());
+console.log(schoolName.trimEnd());
+console.log(schoolName.trimStart());
+
+
+
+
+
+
+
+
+
