1818bf9faff7dc819379795307b8334d902867c3
Lepharam Ramchiary <89240335+LepharamRamchiary@users.noreply.github.com>
2024-09-06 07:42:29 +0000

    math methods

---
+++ 01_js/math.js
@@ -0,0 +1,9 @@
+console.log(Math.random());
+console.log((Math.random() * 10) + 1);
+console.log(Math.floor((Math.random() * 10) + 1));
+
+const min = 10
+const max = 20
+
+const minNumber = Math.floor((Math.random() * (max-min+1)) + min)
+console.log(minNumber);
