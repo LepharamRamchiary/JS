080c32c5807e46e6ff649b109b4536371363ffed
Lepharam Ramchiary <89240335+LepharamRamchiary@users.noreply.github.com>
2024-09-09 09:57:11 +0000

    some methods of array

---
+++ 01_js/array.js
@@ -0,0 +1,57 @@
+// const number = [0,1,2,3,4]
+
+// const num1 = number.push(9)
+// const num1 = number.pop()
+
+// const number2 = [ 7,8,9,10]
+// number.unshift(9)
+// number.shift()
+
+// console.log(number.includes(1))
+// console.log(number)
+// const num1 = number.join()
+// console.log(num1)
+// console.log(num1)
+// console.log(typeof num1)
+// console.log(num1)
+
+
+// const number = [0,1,2,3,4,5,6,7]
+
+// console.log("A ", number);
+
+// console.log(number.slice(1,3))
+
+// console.log("B ", number);
+
+// console.log(number.splice(1,3));
+
+// console.log("c ", number);
+
+// const number = [0,9,2,3,4,5,6,7]
+
+// const isLarges = (e) => e>3
+
+// console.log(number.findIndex(isLarges));
+
+
+// const age = [20, 22, 24, 26]
+
+// const even = (e) => e%2 === 0;
+// const odd = (e) => e%2 !== 0;
+
+// console.log(age.some(odd));
+
+const age = [20, 22, 24, 26]
+
+// const isLarge = (e) => e > 22;
+
+// console.log(age.filter(isLarge));
+// console.log(age);
+
+
+const isLarge = (e) => e > 22
+
+console.log(age.find(isLarge));
+
+
