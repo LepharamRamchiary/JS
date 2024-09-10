20b919b51186a808a55e7ab4fa3dbacb2b598a0b
Lepharam Ramchiary <89240335+LepharamRamchiary@users.noreply.github.com>
2024-09-10 08:50:15 +0000

    add some methods of array

--- 01_js/array.js
+++ 01_js/array.js
@@ -42,7 +42,7 @@
 
 // console.log(age.some(odd));
 
-const age = [20, 22, 24, 26]
+// const age = [20, 22, 24, 26]
 
 // const isLarge = (e) => e > 22;
 
@@ -50,8 +50,33 @@ const age = [20, 22, 24, 26]
 // console.log(age);
 
 
-const isLarge = (e) => e > 22
+// const isLarge = (e) => e > 22
 
-console.log(age.find(isLarge));
+// console.log(age.find(isLarge));
 
+const odd_num = [1, 3, 5, 7, 9]
+const even_num = [2, 4, 6, 8]
+// spret
+const all_num = [...odd_num, ...even_num]
+
+// console.log(all_num);
+
+const another_array = [1, 2, 3, 4, [5, 6, 7], 8, [9, 10, 11, [12, 13, 14, [15, 15]]]]
+
+const real_array = another_array.flat(Infinity) // or we given arr number also
+
+// console.log(real_array);
+
+
+console.log(Array.isArray("Lepharam"));
+console.log(Array.from("Lepharam"));
+console.log(Array.from({ name: "Lepharam" }));
+
+const s1 = 10
+const s2 = 20
+const s3 = 30
+
+console.log(Array.of(s1,s2,s3));
+console.log(Array(7));
+console.log(Array(s1,s2,s3));
 
